f03ca0a1d9063722606155695524dd191e23f88d
Scott Chow <11098209+scchow@users.noreply.github.com>
2023-05-01 10:45:27 -0700

    Added function/file docs

--- lightomatic3000/servo.ino
+++ lightomatic3000/servo.ino
@@ -1,14 +1,32 @@
-//https://docs.arduino.cc/learn/electronics/servo-motors
+/**
+ * @brief Code for interfacing with the servo
+ *
+ * Based on the Arduino Servo library example:
+ * https://docs.arduino.cc/learn/electronics/servo-motors
+ *
+ */
 
-void servo_setup(int servo_pin) {
+/**
+ * @brief Sets the pin provided as a servo pin,
+ *        sets the angle to zero, and waits 0.25 seconds
+ *
+ * @param servo_pin the Digital pin number for the servo
+ */
+void servo_setup(int servo_pin)
+{
   // put your setup code here, to run once:
-  servo.attach(servo_pin);  // attaches the servo on pin 9 to the servo object
-  servo.write(0); // start at home position
+  servo.attach(servo_pin); // attaches the servo on pin 9 to the servo object
+  servo.write(0);          // start at home position
   delay(250);
 }
 
-// Presses the light switch
-void press_button(int angle){
+/**
+ * @brief Presses the light switch by moving the servo to the specified angle
+ *
+ * @param angle the angle (in degrees) to move the servo
+ */
+void press_button(int angle)
+{
   // Move servo to specified angle
   // run_servo(angle);
   servo.write(angle);
--- lightomatic3000/switch.ino
+++ lightomatic3000/switch.ino
@@ -1,9 +1,24 @@
-void switch_setup(int switch_pin, int status_led) {
+
+/**
+ * @brief Setus up the switch and status pins for turning on/off the lightomatic3000
+ *
+ * @param switch_pin the Digital pin number for the switch
+ * @param status_led the Digital pin number for the status LED
+ */
+void switch_setup(int switch_pin, int status_led)
+{
   pinMode(status_led, OUTPUT);
   pinMode(switch_pin, INPUT_PULLUP); // use internal pullup resistor
 }
 
-void switch_loop(int switch_pin, int status_led) {
+/**
+ * @brief Checks if the switch is on and sets the global variable switch_on
+ *
+ * @param switch_pin the Digital pin number for the switch
+ * @param status_led the Digital pin number for the status LED
+ */
+void switch_loop(int switch_pin, int status_led)
+{
   // Check if switch is on
   switch_on = digitalRead(switch_pin) == LOW;
   // illuminate Arduino LED if switch is on
